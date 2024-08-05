2e9cdad0f58e9cadca5c7977e844ea41800f6b4a
Mike MacCana <mike.maccana@gmail.com>
2024-08-05 13:47:39 -0400

    Add bash script used to copy any updated to new location in developer-content

---
+++ new-copy.bash
@@ -0,0 +1,7 @@
+cp cpi-guard.md ../../developer-content/content/courses/token-extensions/
+cp group-member.md ../../developer-content/content/courses/token-extensions/
+cp interest-bearing-token.md ../../developer-content/content/courses/token-extensions/
+cp immutable-owner.md ../../developer-content/content/courses/token-extensions/
+cp permanent-delegate.md ../../developer-content/content/courses/token-extensions/
+cp required-memo.md ../../developer-content/content/courses/token-extensions/
+cp transfer-hook.md ../../developer-content/content/courses/token-extensions/
